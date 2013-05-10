b76d619ef45754ea32b5d7e710cd8d2147b74c29
tcshao <tcshao@gmail.com>
2013-05-10 15:59:59 -0700

    Added solution and tests

---
+++ FizzBuzz-CSharp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    public static class FizzBuzzExtensions
+    {
+        public static bool IsAFactorOf(this int source, int multiple)
+        {
+            return multiple % source == 0;
+        }
+    }
+
+    public class Program
+    {
+        public static string GetFizzBuzz(int number)
+        {
+            if (15.IsAFactorOf(number))
+            {
+                return string.Format("{0} - FizzBuzz", number);
+            }
+
+            if (3.IsAFactorOf(number))
+            {
+                return string.Format("{0} - Fizz", number);
+            }
+
+            if (5.IsAFactorOf(number))
+            {
+                return string.Format("{0} - Buzz", number);
+            }
+            return number.ToString();
+        }
+
+        static void Main()
+        {
+            Enumerable.Range(0, 100)
+                .Select(GetFizzBuzz)
+                .ToList()
+                .ForEach(Console.WriteLine);
+        }
+    }
+}
---
+++ FizzBuzz.Tests/Tests.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FizzBuzz;
+
+namespace FizzBuzz.Tests
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void three_is_a_factor_of_three()
+        {
+            var expected = true;
+            var actual = 3.IsAFactorOf(3);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void three_is_a_factor_of_six()
+        {
+            var expected = true;
+            var actual = 3.IsAFactorOf(6);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void three_is_not_a_factor_of_four()
+        {
+            var expected = false;
+            var actual = 4.IsAFactorOf(3);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void five_is_a_factor_of_five()
+        {
+            var expected = true;
+            var actual = 5.IsAFactorOf(5);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void five_is_a_factor_of_fifteen()
+        {
+            var expected = true;
+            var actual = 5.IsAFactorOf(15);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void three_is_a_factor_of_fifteen()
+        {
+            var expected = true;
+            var actual = 3.IsAFactorOf(15);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void any_number_that_is_a_factor_of_fifteen_is_a_factor_of_three_and_five()
+        {
+            var expected = true;
+            int randomNum = 1;
+
+            while(!15.IsAFactorOf(randomNum))
+            {
+                randomNum = new Random().Next();
+            }
+
+            var actual = 3.IsAFactorOf(randomNum) && 5.IsAFactorOf(randomNum);
+
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod]
+        public void the_number_will_be_returned_as_string_if_it_is_not_a_multiple_of_three_or_five()
+        {
+            int randomNum = 17;
+
+            var expected = randomNum.ToString();
+            var actual = Program.GetFizzBuzz(randomNum);
+
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod]
+        public void a_factor_of_three_will_return_the_number_and_Fizz()
+        {
+            int randomNum = 9;
+
+            var expected = "9 - Fizz";
+            var actual = Program.GetFizzBuzz(randomNum);
+
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod]
+        public void a_factor_of_five_will_return_the_number_and_Buzz()
+        {
+            int randomNum = 20;
+
+            var expected = "20 - Buzz";
+            var actual = Program.GetFizzBuzz(randomNum);
+
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod]
+        public void a_factor_of_fifteen_will_return_the_number_and_FizzBuzz()
+        {
+            int randomNum = 45;
+
+            var expected = "45 - FizzBuzz";
+            var actual = Program.GetFizzBuzz(randomNum);
+
+            Assert.AreEqual(actual, expected);
+        }
+
+
+    }
+}
